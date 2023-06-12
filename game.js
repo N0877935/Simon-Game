129083099798abc51c2cf6d5cead02b0029fa729
Joe Finlay <72800042+N0877935@users.noreply.github.com>
2023-06-12 11:04:46 +0100

    Removed switched statement and refactored code

--- game.js
+++ game.js
@@ -5,42 +5,24 @@ const buttonColours = ['red', 'blue', 'green', 'yellow'];
 
 const randomChosenColour = buttonColours[nextSequence()];
 
-function nextSequence() {
-    const randomNumber = Math.floor(Math.random() * 4);
-    console.log(randomNumber);
-    return randomNumber;
-}
-
-gamePattern.push(randomChosenColour);
-
-$(`#${randomChosenColour}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-
-switch (randomChosenColour) {
-    case 'red':
-        const red = new Audio('./sounds/red.mp3');
-        red.play();
-        break;
-    case 'blue':
-        const blue = new Audio('./sounds/blue.mp3');
-        blue.play();
-        break;
-    case 'green':
-        const green = new Audio('./sounds/green.mp3');
-        green.play();
-        break;
-    case 'yellow':
-        const yellow = new Audio('./sounds/yellow.mp3');
-        yellow.play();
-        break;
-    default:
-
-        break;
-}
 
 $('.btn').on('click', function() {
     const userChosenColour = this.id;
     userClickedPattern.push(userChosenColour);
-    console.log(userClickedPattern);
 })
+
+
+function nextSequence() {
+    const randomNumber = Math.floor(Math.random() * 4);
+    const randomChosenColour = buttonColours[randomNumber];
+
+    gamePattern.push(randomChosenColour);
+
+    $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
+
+}
 
 
